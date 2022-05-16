5adc37ed9137059ce89b5279fab85e0033e310dc
tianshunli <tianshunli@example.com>
2022-05-17 03:05:14 +0800

    5-17号提交

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue lo222go" src="../assets/logo.png">
+    <HelloWorld msg="We1212lc22ome to Your Vue.js App"/>
+    <h2>修改1</h2>
+    <h3>修改2</h3>
+    <h3>修改3</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
